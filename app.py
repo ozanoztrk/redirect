bb4f169c9af9d9d6616f03dd25e27824d5dd1bce
Ozan Ozturk <oztrkozan@gmail.com>
2023-03-02 15:50:56 +0300

    app changed

--- app.py
+++ app.py
@@ -40,4 +40,4 @@ def redirect_page():
 
 if __name__=="__main__":
     port =os.environ.get("PORT",5000)
-    app.run(debug=False,host=0.0.0.0,port=port)
+    app.run(debug=False,host='0.0.0.0',port=port)
